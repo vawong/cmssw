8788edf50a4a33dae312394b7be2d9e299655349
maria <dalfonso@cern.ch>
2016-09-20 09:45:11 +0200

    add the analyzers

---
+++ MakeTimingMaps/plugins/MakePhase1Plots.cc
@@ -0,0 +1,428 @@
+// -*- C++ -*-
+//
+// Package:    HBHETimingValidation/MakeTimingMaps
+// Class:      MakeTimingMaps
+// 
+/**\class MakeTimingMaps MakeTimingMaps.cc HBHETimingValidation/MakeTimingMaps/plugins/MakeTimingMaps.cc
+
+ Description: Quick example code for making HBHE timing monitoring plots
+*/
+//
+// Original Author:  Stephanie Brandt
+//         Created:  Fri, 21 Aug 2015 11:42:17 GMT
+//
+//
+
+
+// system include files
+#include <memory>
+#include <string>
+#include <map>
+#include <iostream>
+using namespace std;
+
+// user include files
+#include "TLine.h"
+#include "TStyle.h"
+#include "TTree.h"
+#include "TFile.h"
+#include "TProfile.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TCanvas.h"
+#include "TProfile2D.h"
+#include "TLatex.h"
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/Framework/interface/Event.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "DataFormats/HcalRecHit/interface/HBHERecHit.h"
+#include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+
+#include "SimDataFormats/CaloHit/interface/PCaloHit.h"
+#include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
+
+#include "SimCalorimetry/HcalSimAlgos/interface/HcalSimParameterMap.h"
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<> and also remove the line from
+// constructor "usesResource("TFileService");"
+// This will improve performance in multithreaded jobs.
+
+class MakePhase1Plots : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+   public:
+      explicit MakePhase1Plots(const edm::ParameterSet&);
+      ~MakePhase1Plots();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+      
+      std::string int2string(int i);
+      
+      void ClearVariables();
+      
+      // some variables for storing information
+      double Method0Energy;
+      double RecHitEnergy;
+      double RecHitTime;
+      int LumiBlock;
+      double iEta;
+      double iPhi; 
+      int depth;
+      int RunNumber;
+      int EvtNumber;
+      
+      // create the output file
+      edm::Service<TFileService> FileService;
+      // create the token to retrieve hit information
+      edm::EDGetTokenT<HBHERecHitCollection>    hRhToken;
+      edm::EDGetTokenT<bool> hIsoToken;
+    
+      TH2F *hCheckEnergyM0MX_barrel;
+      TH2F *hCheckEnergyM0MX_endcap;
+
+      TH1F *hCheckTimingMX_barrel;
+      TH1F *hCheckTimingMX_endcap;
+
+      TH1F *hCheckTimingMX_barrel_gt5;
+      TH1F *hCheckTimingMX_endcap_gt5;
+
+      TH1F *hCheckTimingMX_endcap_depth1_gt5;
+      TH1F *hCheckTimingMX_endcap_depth2_gt5;
+      TH1F *hCheckTimingMX_endcap_depth3_gt5;
+      TH1F *hCheckTimingMX_endcap_depth4_gt5;
+
+      TH2F *hCheckEnergyTiming_barrel;
+      TH2F *hCheckEnergyTiming_endcap;
+
+      TH2F *hCheckT45ratioVST4_barrel;
+      TH2F *hCheckT45ratioVST4_endcap;
+
+      TH1F *hCheckT45ratio_barrel;
+      TH1F *hCheckT45ratio_endcap;
+
+  /*  
+      // 2-d profiles which will hold the average time per channel, 1 for each depth
+      TProfile2D *hHBHETiming_Depth1;
+      TProfile2D *hHBHETiming_Depth2;
+      TProfile2D *hHBHETiming_Depth3;
+      
+      TH2F *occupancy_d1;
+      TH2F *occupancy_d2;
+      TH2F *occupancy_d3;
+      
+      // pointer to pointer to pointers to make a 2-d array of histograms
+      // These are used to hold the individual rechit timing histograms for each channel
+      TH1F ***hTiming_Depth1 = new TH1F**[59];
+      TH1F ***hTiming_Depth2 = new TH1F**[59];
+      TH1F ***hTiming_Depth3 = new TH1F**[6];
+      
+      // Check for correlation between same iphi or adjacent iphi
+      TH1F *hCheckTimingPhi67Plus;
+      TH1F *hCheckTiming66to67P;
+      TH2F *hCorrTimingPhi67Plus;
+      TH2F *hCorrTiming66to67P;
+      
+      // Get energy distributions of channels
+      TH1F *hCheckEnergyIT;
+      TH1F *hCheckEnergyOOT1;
+      TH1F *hCheckEnergyOOT2;
+      
+      TH1F *hCheckEnergyITip51;
+      TH1F *hCheckEnergyOOT1ip51;
+      TH1F *hCheckEnergyOOT2ip51;
+      
+      TH1F *hCheckEnergyITip54;
+      TH1F *hCheckEnergyOOT1ip54;
+      TH1F *hCheckEnergyOOT2ip54;
+  */
+      
+      // Sample histogram for storing time slice information
+      TH1F *hTimeSlices;
+      
+      int runNumber_;
+      double energyCut_;
+      double timeLow_;
+      double timeHigh_;
+      std::vector<double> etas67;
+      std::vector<double> etas66;
+        
+    // crap for trouble-shooting, create a TCanvas here to print out pulse shapes of problem channels
+   TCanvas *c1 = new TCanvas("c1","c1",900,700);
+   int nProblems = 0;
+};
+
+
+MakePhase1Plots::MakePhase1Plots(const edm::ParameterSet& iConfig)
+{
+   //now do what ever initialization is needed
+   usesResource("TFileService");
+
+  // Tell which collection is consumed
+   hRhToken = consumes<HBHERecHitCollection >(iConfig.getUntrackedParameter<string>("HBHERecHits","hbhereco"));
+   //  hRhToken = consumes<HBHERecHitCollection >(iConfig.getUntrackedParameter<string>("HBHERecHits","hbheprereco"));
+  hIsoToken = consumes<bool >(iConfig.getUntrackedParameter<string>("HBHENoiseFilterResultProducer", "HBHEIsoNoiseFilterResult"));
+  
+
+  // example histogram for storing time slice information
+  /*
+  // Get Configurable parameters
+  runNumber_ = iConfig.getParameter<int>("runNumber");
+  energyCut_ = iConfig.getParameter<double>("rechitEnergy");
+  
+  timeLow_ = iConfig.getParameter<double>("timeLowBound");
+  timeHigh_ = iConfig.getParameter<double>("timeHighBound");
+  */
+
+  hCheckEnergyM0MX_barrel = FileService->make<TH2F>("M0MXplot_barrel","M0MXplot_barrel",20,0.,100.,20,0.,100.);
+  hCheckEnergyM0MX_endcap = FileService->make<TH2F>("M0MXplot_endcap","M0MXplot_endcap",20,0.,100.,20,0.,100.);
+
+  hCheckTimingMX_barrel = FileService->make<TH1F>("TimingMX_barrel","TimingMX_barrel",25,-12.5,12.5);
+  hCheckTimingMX_endcap = FileService->make<TH1F>("TimingMX_endcap","TimingMX_endcap",25,-12.5,12.5);
+
+  hCheckTimingMX_barrel_gt5 = FileService->make<TH1F>("TimingMX_barrel_gt5","TimingMX_barrel_gt5",25,-12.5,12.5);
+  hCheckTimingMX_endcap_gt5 = FileService->make<TH1F>("TimingMX_endcap_gt5","TimingMX_endcap_gt5",25,-12.5,12.5);
+
+  hCheckEnergyTiming_barrel = FileService->make<TH2F>("EnergyTiming_barrel","EnergyTiming_barrel",25,-12.5,12.5,20,0.,100.);
+  hCheckEnergyTiming_endcap = FileService->make<TH2F>("EnergyTiming_endcap","EnergyTiming_endcap",25,-12.5,12.5,20,0.,100.);
+
+  hCheckT45ratioVST4_barrel = FileService->make<TH2F>("T45ratioVST4_barrel","T45ratioVST4_barrel", 30,0.,3., 500,0.,500.);
+  hCheckT45ratioVST4_endcap = FileService->make<TH2F>("T45ratioVST4_endcap","T45ratioVST4_endcap", 30,0.,3., 500,0.,500.);
+
+  hCheckT45ratio_barrel = FileService->make<TH1F>("T45ratio_barrel","T45ratio_barrel", 30,0.,3.);
+  hCheckT45ratio_endcap = FileService->make<TH1F>("T45ratio_endcap","T45ratio_endcap", 30,0.,3.);
+
+  hCheckTimingMX_endcap_depth1_gt5 = FileService->make<TH1F>("TimingMX_endcap_depth1_gt5","TimingMX_endcap_depth1_gt5",25,-12.5,12.5);
+  hCheckTimingMX_endcap_depth2_gt5 = FileService->make<TH1F>("TimingMX_endcap_depth2_gt5","TimingMX_endcap_depth2_gt5",25,-12.5,12.5);
+  hCheckTimingMX_endcap_depth3_gt5 = FileService->make<TH1F>("TimingMX_endcap_depth3_gt5","TimingMX_endcap_depth3_gt5",25,-12.5,12.5);
+  hCheckTimingMX_endcap_depth4_gt5 = FileService->make<TH1F>("TimingMX_endcap_depth4_gt5","TimingMX_endcap_depth4_gt5",25,-12.5,12.5);
+
+
+}
+
+
+MakePhase1Plots::~MakePhase1Plots(){} // destructor
+
+// funtion so you can turn an integer into a string for labels and stuff
+std::string MakePhase1Plots::int2string(int i) {
+  stringstream ss;
+  string ret;
+  ss << i;
+  ss >> ret;
+  return ret;
+}
+
+// ------------ method called for each event  ------------
+void
+MakePhase1Plots::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  using namespace edm;
+
+  // Read events
+  Handle<HBHERecHitCollection> hRecHits; // create handle
+  iEvent.getByToken(hRhToken, hRecHits); // get events based on token
+  
+  double TS4=-100;
+  double TS5=0.;
+
+  // Loop over all rechits in one event
+  for(int i = 0; i < (int)hRecHits->size(); i++) {
+    //    ClearVariables(); // sets a bunch of stuff to zero
+    
+    RunNumber = iEvent.id().run(); // get the run number for the event
+    EvtNumber = iEvent.id().event(); // get the event number
+    LumiBlock = iEvent.id().luminosityBlock();
+
+    // Just in case the file you are running over contains events from multiple runs,
+    // remove everything except the run you are interested in
+    //  if(RunNumber != runNumber_) continue;
+    
+    // get ID information for the reconstructed hit
+    HcalDetId detID_rh = (*hRecHits)[i].id().rawId();
+    
+    // ID information can get us detector coordinates
+    depth = (*hRecHits)[i].id().depth();
+    iEta = detID_rh.ieta();
+    iPhi = detID_rh.iphi();
+    
+    // get some variables
+    Method0Energy = (*hRecHits)[i].eraw();
+    RecHitEnergy = (*hRecHits)[i].energy();
+    RecHitTime = (*hRecHits)[i].time();
+
+    if(Method0Energy>5) cout << "energy="<< Method0Energy << "  ; chi2=" << (*hRecHits)[i].getChiSquared() << endl;
+    
+    // this block of code is to extract the charge in individual time slices from 
+    // the auxiliary information 
+                                                                                                                                                                         
+    // Not  used at the moment, but useful for trouble-shooting if we identify a
+    //  problem with some channels
+
+    int adc[10]; 
+
+    int auxwd1 = (*hRecHits)[i].aux();  // TS = 0,1,2,3 info
+    int auxwd2 = (*hRecHits)[i].auxHBHE();      // TS = 4,5,6,7 info
+    int auxwd3 = (*hRecHits)[i].auxPhase1();      // TS = 8,9 info
+
+    // 0x7F --> 7bits 127
+    // 0xFF --> 8bits 255
+
+    // 
+    adc[0] = (auxwd1)       & 0xFF;
+    adc[1] = (auxwd1 >> 8)  & 0xFF;
+    adc[2] = (auxwd1 >> 16) & 0xFF;
+    adc[3] = (auxwd1 >> 24) & 0xFF;
+    
+    adc[4] = (auxwd2)       & 0xFF;
+    adc[5] = (auxwd2 >> 8)  & 0xFF;
+    adc[6] = (auxwd2 >> 16) & 0xFF;
+    adc[7] = (auxwd2 >> 24) & 0xFF;
+
+    adc[8] = (auxwd3 )      & 0xFF;
+    adc[9] = (auxwd3 >> 8)  & 0xFF;
+     
+    TS4=adc[4];
+    TS5=adc[5];    
+
+    // example on how to fill the time slices if you need it
+    if(Method0Energy > 20){
+
+      hTimeSlices = FileService->make<TH1F>(Form("hTimeSlices_%4.2f",Method0Energy),Form("hTimeSlices_%4.2f",Method0Energy),10,-100,150);
+
+      std::cout << "event number is " << EvtNumber << std::endl;
+      std::cout << "iphi, ieta = " << detID_rh.iphi() << " " << detID_rh.ieta() << " time = " << RecHitTime << " energy = " << RecHitEnergy << std::endl;
+      for(int nadc = 0; nadc < 10; nadc++){
+	hTimeSlices->SetBinContent(nadc+1,adc[nadc]);
+	std::cout << adc[nadc] << "  " ;
+      }
+      std::cout << std::endl;
+
+      hTimeSlices->GetXaxis()->SetBinLabel(1,"TS0");
+      hTimeSlices->GetXaxis()->SetBinLabel(2,"TS1");
+      hTimeSlices->GetXaxis()->SetBinLabel(3,"TS2");
+      hTimeSlices->GetXaxis()->SetBinLabel(4,"TS3");
+      hTimeSlices->GetXaxis()->SetBinLabel(5,"TS4");
+      hTimeSlices->GetXaxis()->SetBinLabel(6,"TS5");
+      hTimeSlices->GetXaxis()->SetBinLabel(7,"TS6");
+      hTimeSlices->GetXaxis()->SetBinLabel(8,"TS7");
+      hTimeSlices->GetXaxis()->SetBinLabel(9,"TS8");
+      hTimeSlices->GetXaxis()->SetBinLabel(10,"TS9");
+
+      hTimeSlices->SetTitle("");
+      hTimeSlices->GetYaxis()->SetTitle("tsAdc");
+
+      gStyle->SetOptStat(000000);
+
+      hTimeSlices->SetLineWidth(3);
+      hTimeSlices->Draw();
+
+      ///////////
+      //pedestal Line
+
+      double ped=3;
+      if(std::abs(iEta) < 14) ped=3.;
+      if(std::abs(iEta) >=19  && std::abs(iEta)<=26) ped=17.3;
+
+      TLine *line = new TLine(hTimeSlices->GetXaxis()->GetBinLowEdge(0),ped,hTimeSlices->GetXaxis()->GetBinUpEdge(hTimeSlices->GetXaxis()->GetNbins()),ped);
+      line->SetLineColor(kRed);
+      line->SetLineWidth(3);
+      line->Draw("same");
+
+      ///////////// 
+
+      TLatex *t = new TLatex();
+      t->SetTextFont(32);
+      t->SetTextColor(1);
+      //      t->SetTextSize(0.08);
+      t->SetTextAlign(12);
+      t->SetTextSize(0.05);
+      t->DrawLatex(hTimeSlices->GetXaxis()->GetBinLowEdge(1),1.1*hTimeSlices->GetMaximum(),Form("M0=%4.2f  ieta=%d iphi=%d depth=%d ,    M2=%4.2f",Method0Energy,detID_rh.ieta(),detID_rh.iphi(),depth,RecHitEnergy));
+      //      t->DrawLatex(0.,0.75*hTimeSlices->GetMaximum(),Form("M0=%4.2f",Method0Energy));
+      //      t->DrawLatex(0.15,0.65*hTimeSlices->GetMaximum(),Form("M2=%4.2f",Method2Energy));
+
+      // comment the plotting
+      //      if(std::abs(iEta) < 14) c1->SaveAs(Form("Pulse_Barrel_%d_%d_%d_Energy%4.2f.png",RunNumber,LumiBlock,EvtNumber,Method0Energy));
+      //      if(std::abs(iEta) >=19  && std::abs(iEta)<=26) c1->SaveAs(Form("Pulse_Endcap_%d_%d_%d_Energy%4.2f.png",RunNumber,LumiBlock,EvtNumber,Method0Energy));
+      //("test_phi"+int2string(detID_rh.iphi())+"eta"+int2string(detID_rh.ieta())+"energy"+int2string((int)RecHitEnergy)+"time"+int2string((int)RecHitTime)+".png").c_str());
+      ///       ++nProblems;
+    }
+
+    if(std::abs(iEta) < 14 )  hCheckT45ratioVST4_barrel->Fill(TS5/TS4,TS4);
+    if(std::abs(iEta) >=19  && std::abs(iEta)<=26 )  hCheckT45ratioVST4_endcap->Fill(TS5/TS4,TS4);
+
+    if(std::abs(iEta) < 14 )  hCheckT45ratio_barrel->Fill(TS5/TS4);
+    if(std::abs(iEta) >=19  && std::abs(iEta)<=26 )  hCheckT45ratio_endcap->Fill(TS5/TS4);
+
+//    if(RecHitEnergy > energyCut_)
+    if(std::abs(iEta) < 14 )  hCheckEnergyM0MX_barrel->Fill(Method0Energy,RecHitEnergy);
+    if(std::abs(iEta) >=19  && std::abs(iEta)<=26 )  hCheckEnergyM0MX_endcap->Fill(Method0Energy,RecHitEnergy);
+
+    if(std::abs(iEta) < 14 )  hCheckTimingMX_barrel->Fill(RecHitTime);
+    if(std::abs(iEta) >=19  && std::abs(iEta)<=26 )  hCheckTimingMX_endcap->Fill(RecHitTime);
+
+    if(Method0Energy>5) {
+
+      if(std::abs(iEta) < 14 )  hCheckTimingMX_barrel_gt5->Fill(RecHitTime);
+      if(std::abs(iEta) >=19  && std::abs(iEta)<=26 )  hCheckTimingMX_endcap_gt5->Fill(RecHitTime);
+
+      if( std::abs(iEta) < 14 ) hCheckEnergyTiming_barrel->Fill(RecHitTime,Method0Energy);
+      if( std::abs(iEta) >=19  && std::abs(iEta)<=26  ) hCheckEnergyTiming_endcap->Fill(RecHitTime,Method0Energy);
+
+      if(std::abs(iEta) >=19  && std::abs(iEta)<=26 ) {
+	if(depth==1) hCheckTimingMX_endcap_depth1_gt5->Fill(RecHitTime);
+	if(depth==2) hCheckTimingMX_endcap_depth2_gt5->Fill(RecHitTime);
+	if(depth==3) hCheckTimingMX_endcap_depth3_gt5->Fill(RecHitTime);
+	if(depth==4) hCheckTimingMX_endcap_depth4_gt5->Fill(RecHitTime);
+
+      }
+    }
+
+
+  } // recHit
+
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void MakePhase1Plots::beginJob(){}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void MakePhase1Plots::endJob(){}
+
+void MakePhase1Plots::ClearVariables(){
+ RecHitEnergy = 0;
+ RunNumber = 0;
+ depth=0;
+ iEta = 0;
+ iPhi = 0;
+ RecHitTime = 0;
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void MakePhase1Plots::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  // The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(MakePhase1Plots);
---
+++ MakeTimingMaps/plugins/MakeRun2Plots.cc
@@ -0,0 +1,332 @@
+// -*- C++ -*-
+//
+// Package:    HBHETimingValidation/MakeTimingMaps
+// Class:      MakeTimingMaps
+// 
+/**\class MakeTimingMaps MakeTimingMaps.cc HBHETimingValidation/MakeTimingMaps/plugins/MakeTimingMaps.cc
+
+ Description: Quick example code for making HBHE timing monitoring plots
+*/
+//
+// Original Author:  Stephanie Brandt
+//         Created:  Fri, 21 Aug 2015 11:42:17 GMT
+//
+//
+
+
+// system include files
+#include <memory>
+#include <string>
+#include <map>
+#include <iostream>
+using namespace std;
+
+// user include files
+#include "TLine.h"
+#include "TStyle.h"
+#include "TTree.h"
+#include "TFile.h"
+#include "TProfile.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TCanvas.h"
+#include "TProfile2D.h"
+#include "TLatex.h"
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/Framework/interface/Event.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "DataFormats/HcalRecHit/interface/HBHERecHit.h"
+#include "DataFormats/HcalRecHit/interface/HORecHit.h"
+#include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+
+#include "SimDataFormats/CaloHit/interface/PCaloHit.h"
+#include "SimDataFormats/CaloHit/interface/PCaloHitContainer.h"
+
+#include "SimCalorimetry/HcalSimAlgos/interface/HcalSimParameterMap.h"
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<> and also remove the line from
+// constructor "usesResource("TFileService");"
+// This will improve performance in multithreaded jobs.
+
+class MakeRun2Plots : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+   public:
+      explicit MakeRun2Plots(const edm::ParameterSet&);
+      ~MakeRun2Plots();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+      
+      std::string int2string(int i);
+      
+      void ClearVariables();
+      
+      // some variables for storing information
+      double Method0Energy;
+      double RecHitEnergy;
+      double RecHitTime;
+
+      double Method0EnergyM0;
+      double RecHitEnergyM0;
+      double RecHitTimeM0;
+
+      double Method0EnergyM3;
+      double RecHitEnergyM3;
+      double RecHitTimeM3;
+
+
+      int LumiBlock;
+      double iEta;
+      double iPhi; 
+      int depth;
+      int RunNumber;
+      int EvtNumber;
+      
+      // create the output file
+      edm::Service<TFileService> FileService;
+      // create the token to retrieve hit information
+      edm::EDGetTokenT<HBHERecHitCollection>    hRhTokenM2;
+      edm::EDGetTokenT<HBHERecHitCollection>    hRhTokenM3;
+      edm::EDGetTokenT<HBHERecHitCollection>    hRhTokenM0;
+      edm::EDGetTokenT<HORecHitCollection>    hOToken;
+
+      TH1F *hCheckTimingM2;
+      TH1F *hCheckEnergyM2;
+
+      TH1F *hCheckTimingM3;
+      TH1F *hCheckEnergyM3;
+
+      TH1F *hCheckTimingM0;
+      TH1F *hCheckEnergyM0;
+    
+      TH1F *hCheckTimingHO;
+
+      TH2F *hCheckEnergyM2M3;
+
+};
+
+
+MakeRun2Plots::MakeRun2Plots(const edm::ParameterSet& iConfig)
+{
+  //now do what ever initialization is needed
+  usesResource("TFileService");
+  
+  // Tell which collection is consumed
+  hRhTokenM2 = consumes<HBHERecHitCollection >(iConfig.getUntrackedParameter<string>("HBHERecHits","hbhereco"));
+  hRhTokenM3 = consumes<HBHERecHitCollection >(iConfig.getUntrackedParameter<string>("HBHERecHits","hbherecoM3"));
+  hRhTokenM0 = consumes<HBHERecHitCollection >(iConfig.getUntrackedParameter<string>("HBHERecHits","hbherecoM0"));
+
+  hOToken = consumes<HORecHitCollection >(iConfig.getUntrackedParameter<string>("HORecHits","horeco"));
+
+  // example histogram for storing time slice information
+  /*
+  // Get Configurable parameters
+  runNumber_ = iConfig.getParameter<int>("runNumber");
+  energyCut_ = iConfig.getParameter<double>("rechitEnergy");
+  
+  timeLow_ = iConfig.getParameter<double>("timeLowBound");
+  timeHigh_ = iConfig.getParameter<double>("timeHighBound");
+  */
+
+
+  hCheckTimingHO = FileService->make<TH1F>("TimingHO","TimingHO",100,60.,40.);
+
+  hCheckTimingM2 = FileService->make<TH1F>("TimingM2","TimingM2",25,-12.5,12.5);
+  hCheckEnergyM2 = FileService->make<TH1F>("EnergyM2","EnergyM2",20,0.,100.);
+
+  hCheckTimingM0 = FileService->make<TH1F>("TimingM0","TimingM0",25,-12.5,12.5);
+  hCheckEnergyM0 = FileService->make<TH1F>("EnergyM0","EnergyM0",20,0.,100.);
+
+  hCheckTimingM3 = FileService->make<TH1F>("TimingM3","TimingM3",25,-12.5,12.5);
+  hCheckEnergyM3 = FileService->make<TH1F>("EnergyM3","EnergyM3",20,0.,100.);
+
+  hCheckEnergyM2M3 = FileService->make<TH2F>("EnergyM2M3","EnergyM2M3",20,0.,100.,20,0.,100.);
+
+}
+
+
+MakeRun2Plots::~MakeRun2Plots(){} // destructor
+
+// ------------ method called for each event  ------------
+void
+MakeRun2Plots::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  using namespace edm;
+
+  // Read events
+  Handle<HBHERecHitCollection> hRecHits; // create handle
+  iEvent.getByToken(hRhTokenM2, hRecHits); // get events based on token
+
+  Handle<HBHERecHitCollection> hRecHitsM3; // create handle
+  iEvent.getByToken(hRhTokenM3, hRecHitsM3); // get events based on token
+
+  Handle<HBHERecHitCollection> hRecHitsM0; // create handle
+  iEvent.getByToken(hRhTokenM0, hRecHitsM0); // get events based on token
+
+  Handle<HORecHitCollection> hRecHitsHO; // create handle
+  iEvent.getByToken(hOToken, hRecHitsHO); // get events based on token
+
+  RunNumber = iEvent.id().run(); // get the run number for the event
+  EvtNumber = iEvent.id().event(); // get the event number
+  LumiBlock = iEvent.id().luminosityBlock();
+  
+  // Loop over all rechits in one event
+  for(int i = 0; i < (int)hRecHits->size(); i++) {
+    //    ClearVariables(); // sets a bunch of stuff to zero
+
+    // Just in case the file you are running over contains events from multiple runs,
+    // remove everything except the run you are interested in
+    //  if(RunNumber != runNumber_) continue;
+    
+    // get ID information for the reconstructed hit
+    HcalDetId detID_rh = (*hRecHits)[i].id().rawId();
+    
+    // ID information can get us detector coordinates
+    depth = (*hRecHits)[i].id().depth();
+    iEta = detID_rh.ieta();
+    iPhi = detID_rh.iphi();
+    
+    // get some variables
+    Method0Energy = (*hRecHits)[i].eraw();
+    RecHitEnergy = (*hRecHits)[i].energy();
+    RecHitTime = (*hRecHits)[i].time();
+
+    hCheckTimingM2->Fill(RecHitTime);
+    hCheckEnergyM2->Fill(RecHitEnergy);
+
+  } // recHit
+
+  ///////////
+  /// $$$$
+  /// $$$$
+  ///
+
+  // Loop over all rechits in one event
+  for(int i = 0; i < (int)hRecHitsM0->size(); i++) {
+    //    ClearVariables(); // sets a bunch of stuff to zero
+    
+    // Just in case the file you are running over contains events from multiple runs,
+    // remove everything except the run you are interested in
+    //  if(RunNumber != runNumber_) continue;
+    
+    // get ID information for the reconstructed hit
+    HcalDetId detID_rh = (*hRecHitsM0)[i].id().rawId();
+    
+    // ID information can get us detector coordinates
+    depth = (*hRecHitsM0)[i].id().depth();
+    iEta = detID_rh.ieta();
+    iPhi = detID_rh.iphi();
+    
+    // get some variables
+    Method0EnergyM0 = (*hRecHitsM0)[i].eraw();
+    RecHitEnergyM0 = (*hRecHitsM0)[i].energy();
+    RecHitTimeM0 = (*hRecHitsM0)[i].time();
+
+    hCheckTimingM0->Fill(RecHitTime);
+    hCheckEnergyM0->Fill(RecHitEnergy);
+
+  } // recHitM0
+
+
+  // Loop over all rechits in one event
+  for(int i = 0; i < (int)hRecHitsM3->size(); i++) {
+    //    ClearVariables(); // sets a bunch of stuff to zero
+    
+    // Just in case the file you are running over contains events from multiple runs,
+    // remove everything except the run you are interested in
+    //  if(RunNumber != runNumber_) continue;
+    
+    // get ID information for the reconstructed hit
+    HcalDetId detID_rh = (*hRecHitsM3)[i].id().rawId();
+    
+    // ID information can get us detector coordinates
+    depth = (*hRecHitsM3)[i].id().depth();
+    iEta = detID_rh.ieta();
+    iPhi = detID_rh.iphi();
+    
+    // get some variables
+    Method0EnergyM3 = (*hRecHitsM3)[i].eraw();
+    RecHitEnergyM3 = (*hRecHitsM3)[i].energy();
+    RecHitTimeM3 = (*hRecHitsM3)[i].time();
+
+    hCheckTimingM3->Fill(RecHitTime);
+    hCheckEnergyM3->Fill(RecHitEnergy);
+
+  } // recHit M3
+
+  ///// checking HO
+
+  // Loop over all rechits in one event
+  for(int i = 0; i < (int)hRecHitsHO->size(); i++) {
+    //    ClearVariables(); // sets a bunch of stuff to zero
+    
+    // Just in case the file you are running over contains events from multiple runs,
+    // remove everything except the run you are interested in
+    //  if(RunNumber != runNumber_) continue;
+    
+    // get ID information for the reconstructed hit
+    HcalDetId detID_rh = (*hRecHitsHO)[i].id().rawId();
+    
+    // ID information can get us detector coordinates
+    depth = (*hRecHitsHO)[i].id().depth();
+    iEta = detID_rh.ieta();
+    iPhi = detID_rh.iphi();
+    
+    // get some variables
+    //    Method0EnergyM0 = (*hRecHitsHO)[i].eraw();
+    //    RecHitEnergyM0 = (*hRecHitsM0)[i].energy();
+    //    RecHitTimeM0 = (*hRecHitsM0)[i].time();
+
+    hCheckTimingHO->Fill((*hRecHitsHO)[i].time());
+
+  } // recHitM0
+
+
+
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void MakeRun2Plots::beginJob(){}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void MakeRun2Plots::endJob(){}
+
+void MakeRun2Plots::ClearVariables(){
+ RecHitEnergy = 0;
+ RunNumber = 0;
+ depth=0;
+ iEta = 0;
+ iPhi = 0;
+ RecHitTime = 0;
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void MakeRun2Plots::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  // The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(MakeRun2Plots);
